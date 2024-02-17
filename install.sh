8dc5d6984e17dd185f4361b9cfc8518767bc18b7
Rodrigo Domingues da Silva <rodrigodominguessilva@gmail.com>
2024-02-16 22:54:20 -0300

    implementação instalador

--- install.sh
+++ install.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
+
+
+if [ -z "$BASH" ]; then
+    echo "Este script requer o Bash para execução. Por favor, execute com bash."
+    echo "Exemplo:"
+    echo "bash install.sh"
+    exit 1
+fi
+
+
 echo simplesmente sera copiado os dois scripts pdftoosdividir e pdftoosmesclar para /usr/bin
 cd scripts
 sudo cp * /usr/bin/
 sudo chmod +x /usr/bin/pdftoosdividir.sh
 sudo chmod +x /usr/bin/pdftoosmesclar.sh
-sudo apt install pdftk
+
+
+# Verifica se o pdftk está instalado
+if ! dpkg -l | grep -qw pdftk; then
+    echo "pdftk não está instalado. Tentando instalar..."
+
+    # Atualiza a lista de pacotes
+    sudo apt-get update
+
+    # Tenta instalar o pdftk
+    if sudo apt-get install -y pdftk; then
+        echo "pdftk instalado com sucesso."
+    else
+        echo "Não foi possível encontrar o pacote pdftk nos repositórios padrão."
+        echo "Você pode tentar instalar 'pdftk-java' como uma alternativa ou procurar um pacote '.deb' do pdftk online."
+    fi
+else
+    echo "pdftk já está instalado."
+fi
+
